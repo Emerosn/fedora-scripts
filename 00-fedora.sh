0f72738e492d8405103b70557f2a4387963f6ea9
Emerson <31738756+Emerosn@users.noreply.github.com>
2019-08-31 22:04:16 -0300

    Create 00-fedora.sh

---
+++ 00-fedora.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Repositorio rpmfusion para Nvidia
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+#Pacotes do meu interesse
+dnf install steam \
+	    nvidia-settings.x86_64\
+	    nano\
+	    lutris\
+	    wine\
+	    vulkan-loader.x86_64\
+	    mesa-vulkan-drivers.x86_64\
+            vulkan-tools.x86_64\
+	    xorg-x11-drv-nvidia\
+            akmod-nvidia\ 
+	    nvidia-xconfig.x86_64\
+ 	    nvidia-settings.x86_64
